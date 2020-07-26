21e94788f6dad7f08a1af319c573f5af60f8852d
wajeehamanzoor <wajeehamanzoor97@gmail.com>
2020-07-26 23:39:19 +0500

    updated

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const students = new Schema({
-    // _id: '',
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    gender: { type: String, required: true },
-    join_date: { type: Date, default: Date.now() }
+  // _id: '',
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  gender: { type: String, required: true },
+  join_date: { type: Date, default: Date.now() }
 });
 
 // students.pre('save', function () {
@@ -21,29 +21,41 @@ const StudentSchema = mongoose.model('students', students);
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  const students= await StudentSchema.find({});
-  res.render('index', { title: 'Express', students });
+  const students = await StudentSchema.find({});
+  res.render('index', { title: 'Add Student', students });
 });
+router.get('/add', async function (req, res, next) {
+  res.render('addStudent', { title: 'Add Student', students });
 
-router.post('/', async (req, res)=>{
+  
+});
+router.post('/', async (req, res) => {
+  let student = null
   const studentObj = new StudentSchema({
     first_name: req.body.first_name,
     last_name: req.body.last_name,
     gender: req.body.gender
   })
-  const student = await studentObj.save();
-  if(student) res.status(201).send('OK');
-  else res.status(500).send('FAILED');
+  if(studentObj.first_name && studentObj.last_name && studentObj.gender)
+    {
+      let gender = (studentObj.gender).toLowerCase()
+      if (gender === 'female' || gender === 'male' || gender === 'other')
+        student = await studentObj.save();
+      else
+        return res.status(500).send('Invalid Gender');
+    }
+  else
+    return res.status(500).send('All Fields must be filled');
+  if (student) return res.status(201).send('OK');
 })
 
 router.get('/:id', function (req, res) {
   const id = req.params.id;
-  StudentSchema.findById(id, function (err, student) {
+  StudentSchema.findById({ _id: id }, function (err, student) {
     if (err)
-      res.send(err)
-
-    res.json(student);
-    // res.send("I am here")
+    return res.status(500).send('Student Not Found');
+    // res.json(student);
+    res.send("I am here")
 
   });
 
@@ -56,8 +68,9 @@ router.put('/:id', (req, res) => {
     last_name: req.body.last_name,
     gender: req.body.gender
   }
-  StudentSchema.findByIdAndUpdate({_id:id}, studentObj, function (err, StudentSchema) {
-    if (err) res.status(500).send('FAILED');
+  StudentSchema.findByIdAndUpdate({ _id: id }, studentObj, function (err, student) {
+    if (err) return res.status(500).send('Student Not Found');
+
 
     res.send('Successfully! Student updated - ' + studentObj.first_name + ' ' + studentObj.last_name);
   })
@@ -68,7 +81,8 @@ router.delete('/:id', function (req, res) {
   const id = req.params.id;
   StudentSchema.findOneAndDelete({ _id: id }, function (err, student) {
     if (err)
-      res.status(500).send('FAILED');
+      return res.status(500).send('FAILED');
+
 
     if (!student)
       res.status(404).send("Student Not Found")
@@ -80,4 +94,3 @@ router.delete('/:id', function (req, res) {
 
 module.exports = router;
 
-module.exports = router;
